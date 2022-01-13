8717399c546ed8844732cef37fa987e258c7848d
zhong-my <zhongmingyang2000@gmail.com>
2022-01-13 23:59:41 +0800

    update easy type of leetcode

--- easy.go
+++ easy.go
@@ -764,6 +764,17 @@ func singleNumber2(nums []int) int {
  *     Next *ListNode
  * }
  */
-// func hasCycle(head *ListNode) bool {
-
-// }
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	p1, p2 := head, head.Next
+	for p1 != p2 {
+		if p2 == nil || p2.Next == nil {
+			return false
+		}
+		p1 = p1.Next
+		p2 = p2.Next.Next
+	}
+	return true
+}
